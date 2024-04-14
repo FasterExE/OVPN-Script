75ae445c5e265fbc6afa54acea77a1d36cf83034
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-14 06:01:59 -0700

    Add files via upload

---
+++ data/install_apache2.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#Script Variables
+PORT_TCP='1194'
+PORT_UDP='2200'
+
+clear
+apt install apache2 -y
+rm /etc/apache2/ports.conf
+wget -O /etc/apache2/ports.conf https://raw.githubusercontent.com/FasterExE/OVPN-Script/main/data/apache2.port
+sudo systemctl restart apache2
+clear
+rm /var/www/html/udp.ovpn
+rm /var/www/html/tcp.ovpn
+touch /var/www/html/udp.ovpn
+touch /var/www/html/tcp.ovpn
+echo "client
+dev tun
+proto udp
+port 2200
+remote $(hostname -I | awk '{print $1}')
+cipher none
+auth none
+auth-user-pass
+ping-timer-rem
+redirect-gateway def1
+setenv CLIENT_CERT 0
+verb 3
+<ca>
+-----BEGIN CERTIFICATE-----
+MIICMTCCAZqgAwIBAgIUAaQBApMS2dYBqYPcA3Pa7cjjw7cwDQYJKoZIhvcNAQEL
+BQAwDzENMAsGA1UEAwwES29iWjAeFw0yMDA3MjIyMjIzMzNaFw0zMDA3MjAyMjIz
+MzNaMA8xDTALBgNVBAMMBEtvYlowgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGB
+AMF46UVi2O5pZpddOPyzU2EyIrr8NrpXqs8BlYhUjxOcCrkMjFu2G9hk7QIZ4qO0
+GWVZpPhYk5qWk+LxCsryrSoe0a5HaqIye8BFJmXV0k+O/3e6k06UGNii3gxBWQpF
+7r/2CyQLus9OSpQPYszByBvtkwiBAo/V98jdpm+EVu6tAgMBAAGjgYkwgYYwHQYD
+VR0OBBYEFGRJMm/+ZmLxV027kahdvSY+UaTSMEoGA1UdIwRDMEGAFGRJMm/+ZmLx
+V027kahdvSY+UaTSoROkETAPMQ0wCwYDVQQDDARLb2JaghQBpAECkxLZ1gGpg9wD
+c9rtyOPDtzAMBgNVHRMEBTADAQH/MAsGA1UdDwQEAwIBBjANBgkqhkiG9w0BAQsF
+AAOBgQC0f8wb5hyEOEEX64l8QCNpyd/WLjoeE5bE+xnIcKE+XpEoDRZwugLoyQdc
+HKa3aRHNqKpR7H696XJReo4+pocDeyj7rATbO5dZmSMNmMzbsjQeXux0XjwmZIHu
+eDKMefDi0ZfiZmnU2njmTncyZKxv18Ikjws0Myc8PtAxy2qdcA==
+-----END CERTIFICATE-----
+</ca>" > /var/www/html/udp.ovpn
+
+
+echo "client
+dev tun
+proto tcp
+port 1194
+remote $(hostname -I | awk '{print $1}')
+cipher none
+auth none
+auth-user-pass
+ping-timer-rem
+redirect-gateway def1
+setenv CLIENT_CERT 0
+verb 3
+<ca>
+-----BEGIN CERTIFICATE-----
+MIICMTCCAZqgAwIBAgIUAaQBApMS2dYBqYPcA3Pa7cjjw7cwDQYJKoZIhvcNAQEL
+BQAwDzENMAsGA1UEAwwES29iWjAeFw0yMDA3MjIyMjIzMzNaFw0zMDA3MjAyMjIz
+MzNaMA8xDTALBgNVBAMMBEtvYlowgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGB
+AMF46UVi2O5pZpddOPyzU2EyIrr8NrpXqs8BlYhUjxOcCrkMjFu2G9hk7QIZ4qO0
+GWVZpPhYk5qWk+LxCsryrSoe0a5HaqIye8BFJmXV0k+O/3e6k06UGNii3gxBWQpF
+7r/2CyQLus9OSpQPYszByBvtkwiBAo/V98jdpm+EVu6tAgMBAAGjgYkwgYYwHQYD
+VR0OBBYEFGRJMm/+ZmLxV027kahdvSY+UaTSMEoGA1UdIwRDMEGAFGRJMm/+ZmLx
+V027kahdvSY+UaTSoROkETAPMQ0wCwYDVQQDDARLb2JaghQBpAECkxLZ1gGpg9wD
+c9rtyOPDtzAMBgNVHRMEBTADAQH/MAsGA1UdDwQEAwIBBjANBgkqhkiG9w0BAQsF
+AAOBgQC0f8wb5hyEOEEX64l8QCNpyd/WLjoeE5bE+xnIcKE+XpEoDRZwugLoyQdc
+HKa3aRHNqKpR7H696XJReo4+pocDeyj7rATbO5dZmSMNmMzbsjQeXux0XjwmZIHu
+eDKMefDi0ZfiZmnU2njmTncyZKxv18Ikjws0Myc8PtAxy2qdcA==
+-----END CERTIFICATE-----
+</ca>" > /var/www/html/tcp.ovpn
+
+clear
