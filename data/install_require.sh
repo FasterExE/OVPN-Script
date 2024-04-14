bc157aa211f833b1eac132d18abf34261dc07f5f
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-14 15:28:38 +0100

    Update install_require.sh

--- data/install_require.sh
+++ data/install_require.sh
@@ -8,6 +8,7 @@ apt install -y curl wget cron python-minimal libpython-stdlib
 apt install -y iptables
 apt install -y openvpn netcat neofetch vnstat
 apt install -y screen stunnel4 gnutls-bin python
+apt install -y nload
 apt install -y dos2unix nano unzip jq virt-what net-tools
 apt install -y mlocate dh-make libaudit-dev build-essential fail2ban
 apt-get -o Acquire::ForceIPv4=true install python dos2unix stunnel4 screen curl -y
