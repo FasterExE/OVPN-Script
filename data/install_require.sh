7ba1e669f695f37f72741691a5393de590991b35
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-13 21:40:55 -0700

    Add files via upload

--- data/install_require.sh
+++ data/install_require.sh
@@ -3,14 +3,11 @@
 PORT_TCP='1194'
 PORT_UDP='2200'
 
-clear
-echo 'Installing dependencies.'
-export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install -y curl wget cron python-minimal libpython-stdlib
 apt install -y iptables
-apt install -y openvpn vnstat
-apt install -y stunnel4 gnutls-bin python
+apt install -y openvpn netcat httpie php neofetch vnstat
+apt install -y screen stunnel4 gnutls-bin python
 apt install -y dos2unix nano unzip jq virt-what net-tools default-mysql-client
 apt install -y mlocate dh-make libaudit-dev build-essential fail2ban
 apt-get -o Acquire::ForceIPv4=true install python dos2unix stunnel4 screen curl -y
