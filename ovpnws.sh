c8b8e7666b0d36f7f15b1c79fff5c912fd2c9e97
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-14 03:02:23 +0100

    Update ovpnws.sh

--- ovpnws.sh
+++ ovpnws.sh
@@ -472,17 +472,6 @@ sudo service stunnel4 restart
   } &>/dev/null
 }
 
-installBBR() {
-    echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-    echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-    sysctl -p
-    
-    apt install -y linux-generic-hwe-20.04
-    grub-set-default 0
-    echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
-    INSTALL_BBR=true
-}
-
 install_rclocal(){
   {
   sed -i 's/Listen 80/Listen 81/g' /etc/apache2/ports.conf
