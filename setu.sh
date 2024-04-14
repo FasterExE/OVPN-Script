bc98e8ba3b5ad8b126b9ab5cc50b8f4863b5e6f6
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-14 20:23:13 +0100

    Update setu.sh

--- setu.sh
+++ setu.sh
@@ -18,8 +18,7 @@ echo -e "\e[38;5;208m      GitHub: \e[32mhttps://github.com/FasterExE\e[0m"
 echo -e "\e[38;5;208m      Telegram: \e[32mhttps://t.me/IlyassExE\e[0m"
 echo -e "\033[1;36m──────────────────────────────────────────────────────────\033[0m"
 
-
-echo -e "${DARK_ORANGE}            [${NO_COLOR}${DARK_BLUE}1${DARK_ORANGE}]${NO_COLOR} ${LIGHT_YELLOW}INSTALL REQUIRE${NO_COLOR}"
+echo -e "${DARK_ORANGE}                 [${NO_COLOR}${DARK_BLUE}1${DARK_ORANGE}]${NO_COLOR} ${LIGHT_YELLOW}INSTALL REQUIRE${NO_COLOR}"
 wget https://raw.githubusercontent.com/FasterExE/OVPN-Script/main/data/install_require.sh
 bash install_require.sh
 rm install_require.sh
