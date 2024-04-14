2f1eeb9862c885adee6f00baf05f973de85f9b49
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-14 06:06:49 +0100

    Update install_require.sh

--- data/install_require.sh
+++ data/install_require.sh
@@ -6,9 +6,9 @@ PORT_UDP='2200'
 apt update
 apt install -y curl wget cron python-minimal libpython-stdlib
 apt install -y iptables
-apt install -y openvpn netcat httpie php neofetch vnstat
+apt install -y openvpn netcat neofetch vnstat
 apt install -y screen stunnel4 gnutls-bin python
-apt install -y dos2unix nano unzip jq virt-what net-tools default-mysql-client
+apt install -y dos2unix nano unzip jq virt-what net-tools
 apt install -y mlocate dh-make libaudit-dev build-essential fail2ban
 apt-get -o Acquire::ForceIPv4=true install python dos2unix stunnel4 screen curl -y
 clear
