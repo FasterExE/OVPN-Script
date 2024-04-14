0b298010916a60967c92c287939394b0d9517249
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-14 03:00:34 +0100

    Update ovpnws.sh

--- ovpnws.sh
+++ ovpnws.sh
@@ -7,16 +7,20 @@ timedatectl set-timezone Asia/Riyadh
 install_require () {
 clear
 echo 'Installing dependencies.'
+{
 export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install -y curl wget cron python-minimal libpython-stdlib
 apt install -y iptables
-apt install -y openvpn netcat vnstat
-apt install -y screen stunnel4 gnutls-bin python
+apt install -y openvpn vnstat
+apt install -y stunnel4 gnutls-bin python
 apt install -y dos2unix nano unzip jq virt-what net-tools default-mysql-client
 apt install -y mlocate dh-make libaudit-dev build-essential fail2ban
+apt-get -o Acquire::ForceIPv4=true install python dos2unix stunnel4 screen curl -y
 clear
 }&>/dev/null
+clear
+}
 
 install_socks()
 {
