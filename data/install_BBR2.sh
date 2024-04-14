34909730bd8421697d4beda5ce8b12b1abbee4c2
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-14 20:48:23 +0100

    Update install_BBR2.sh

--- data/install_BBR2.sh
+++ data/install_BBR2.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+#Script Variables
+PORT_TCP='1194'
+PORT_UDP='2200'
 
+ sudo iptables -t nat -A POSTROUTING -o eth0 -s 10.20.0.0/16 -j MASQUERADE
+ sudo iptables -t nat -A POSTROUTING -o eth0 -s 10.30.0.0/16 -j MASQUERADE
