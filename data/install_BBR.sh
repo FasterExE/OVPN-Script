e62cf57ff97540e68183047c73184ea3c6180dc7
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-14 20:45:48 +0100

    Update install_BBR.sh

--- data/install_BBR.sh
+++ data/install_BBR.sh
@@ -14,6 +14,5 @@ sysctl -p
  sudo sysctl -p
  sudo iptables -t nat -A POSTROUTING -o eth0 -s 10.20.0.0/16 -j MASQUERADE
  sudo iptables -t nat -A POSTROUTING -o eth0 -s 10.30.0.0/16 -j MASQUERADE
- sudo apt-get install iptables-persistent -y
  sudo iptables -t nat -A POSTROUTING -o eth0 -s 10.20.0.0/16 -j MASQUERADE
  sudo iptables -t nat -A POSTROUTING -o eth0 -s 10.30.0.0/16 -j MASQUERADE
