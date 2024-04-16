a183a3d0b6fce938d29f388b9ab43f3f1dca2183
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-16 19:02:44 +0100

    Update telegram-bot.sh

--- data/telegram-bot.sh
+++ data/telegram-bot.sh
@@ -2,8 +2,8 @@ MYIP=$(curl -sS ipv4.icanhazip.com)
 izinsc="https://raw.githubusercontent.com/FasterExE/OVPN-Script/main/premission/vps-access"
 rm -f /usr/bin/user
 username=$(curl $izinsc | grep $MYIP | awk '{print $2}') > /dev/null 2>&1
-echo "$username" >/usr/bin/user
-exp=$(curl $izinsc | grep $MYIP | awk '{print $3}')
+echo "$username" >/usr/bin/user > /dev/null 2>&1
+exp=$(curl $izinsc | grep $MYIP | awk '{print $3}') > /dev/null 2>&1
 echo "$exp" >/usr/bin/e
 Name=$(cat /usr/bin/user)
 Exp=$(cat /usr/bin/e)
