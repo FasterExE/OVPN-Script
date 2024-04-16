34735b56c727db865e5e030bcff8480bea42bf49
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-16 18:09:02 +0100

    Update install_menu.sh

--- data/install_menu.sh
+++ data/install_menu.sh
@@ -9,6 +9,7 @@ rm /usr/local/bin/menu
 rm /usr/local/bin/add-user
 rm /usr/local/bin/del-user
 rm /usr/local/bin/on-user
+rm /etc/version
 wget -O /usr/local/bin/menu https://raw.githubusercontent.com/FasterExE/OVPN-Script/main/data/menu
 chmod +x /usr/local/bin/menu
 wget -O /usr/local/bin/add-user https://raw.githubusercontent.com/FasterExE/OVPN-Script/main/data/add-user
@@ -17,4 +18,5 @@ wget -O /usr/local/bin/del-user https://raw.githubusercontent.com/FasterExE/OVPN
 chmod +x /usr/local/bin/del-user
 wget -O /usr/local/bin/on-user https://raw.githubusercontent.com/FasterExE/OVPN-Script/main/data/on-user
 chmod +x /usr/local/bin/on-user
+wget -O /etc/version https://raw.githubusercontent.com/FasterExE/OVPN-Script/main/version
 clear
