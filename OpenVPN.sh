db4d0c106dee071d96a2227657455ba72c0330ce
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-04-21 13:39:56 -0700

    Add files via upload

--- OpenVPN.sh
+++ OpenVPN.sh
@@ -7,8 +7,6 @@ CYAN='\033[0;36m'
 RED='\033[0;31m'
 NC='\033[0m'
 clear
-rm setup1.sh > /dev/null 2>&1
-rm setup2.sh > /dev/null 2>&1
 echo -e "${ORANGE}"
 echo " ██████╗ ██████╗ ███████╗███╗   ██╗    ██╗   ██╗██████╗ ███╗   ██╗"
 echo "██╔═══██╗██╔══██╗██╔════╝████╗  ██║    ██║   ██║██╔══██╗████╗  ██║"
@@ -24,7 +22,44 @@ display_options() {
     echo -e "${GREEN}2${NC}) ${BLUE}Install OpenVPN V3${NC}"
     echo -e "${RED}x${NC}) ${RED}Exit${NC}"
 }
-sleep 2
-rm setup.sh > /dev/null 2>&1; wget https://raw.githubusercontent.com/FasterExE/OVPN-Script-V3/main/setup.sh; bash setup.sh
-rm setup.sh > /dev/null 2>&1
-rm OpenVPN.sh > /dev/null 2>&1
+
+display_options
+
+while true; do
+    echo -n -e "${ORANGE}Enter the number of the command (or 'x' to exit): ${NC}"
+    read choice
+
+    case $choice in
+        1)
+            echo -e "${ORANGE}Installing OpenVPN Classic...${NC}"
+            clear
+            wget https://raw.githubusercontent.com/FasterExE/OVPN-Script-Classic/main/setup1.sh > /dev/null 2>&1
+            if [ $? -eq 0 ]; then
+                bash setup1.sh
+                rm setup1.sh > /dev/null 2>&1
+            else
+                echo -e "${RED}Download failed.${NC}"
+            fi
+            ;;
+        2)
+            echo -e "${ORANGE}Installing OpenVPN V3...${NC}"
+            clear
+            wget https://raw.githubusercontent.com/FasterExE/OVPN-Script-V3/main/setup2.sh > /dev/null 2>&1
+            if [ $? -eq 0 ]; then
+                bash setup2.sh
+                rm setup2.sh > /dev/null 2>&1
+            else
+                echo -e "${RED}Download failed.${NC}"
+            fi
+            ;;
+        x|X)
+            echo -e "${ORANGE}Exiting...${NC}"
+            break
+            ;;
+        *)
+            echo -e "${RED}Invalid choice. Please select a valid option.${NC}"
+            display_options
+            ;;
+    esac
+done
+
